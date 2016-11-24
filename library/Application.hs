4f560d92b7ac507e6d78b0b280e5f67aa0b51d8b
Sandrass <sandranicky@gmail.com>
2016-11-24 00:38:12 +0000

    Pasta templates e mais algumas configurações

--- library/Application.hs
+++ library/Application.hs
@@ -10,9 +10,6 @@ import Handler.Usuario
 import Handler.Contatos
 import Handler.Livro
 import Handler.Categorias
-import View.Home
-import View.Institucional
-import View.Duvidas
 
 import Database.Persist.Postgresql
 ------------------
@@ -20,7 +17,71 @@ mkYesodDispatch "App" resourcesApp
 
 
 
-
+getHomeR:: Handler Html
+getHomeR = defaultLayout $ do
+    sess <- lookupSession "_ID"
+    setTitle "Home / Biblioteca do Saber"
+    
+    toWidgetHead[hamlet|
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    |]
+    -- Adiciona o bootstrap via CDN
+    addStylesheetRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    
+    -- Adiciona o jquery via CDN
+    addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"
+    -- Adiciona o js via CDN
+    addScriptRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+    
+    addStylesheet $ StaticR css_principal_css
+    
+    $(whamletFile "templates/nav.hamlet")
+    $(whamletFile "templates/home.hamlet")
+    $(whamletFile "templates/footer.hamlet")
     
     
+getInstR:: Handler Html
+getInstR = defaultLayout $ do
+    sess <- lookupSession "_ID"
+    setTitle "Institucional"
     
+    toWidgetHead[hamlet|
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    |]
+    -- Adiciona o bootstrap via CDN
+    addStylesheetRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    
+    -- Adiciona o jquery via CDN
+    addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"
+    -- Adiciona o js via CDN
+    addScriptRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+    
+    addStylesheet $ StaticR css_principal_css    
+    $(whamletFile "templates/nav.hamlet")
+    $(whamletFile "templates/institucional.hamlet")
+    $(whamletFile "templates/footer.hamlet")
+    
+getDuvR:: Handler Html
+getDuvR = defaultLayout $ do
+    sess <- lookupSession "_ID"
+    setTitle "Duvidas / Biblioteca do Saber"
+    
+    toWidgetHead[hamlet|
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    |]
+    -- Adiciona o bootstrap via CDN
+    addStylesheetRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+    
+    -- Adiciona o jquery via CDN
+    addScriptRemote "https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"
+    -- Adiciona o js via CDN
+    addScriptRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+    
+    addStylesheet $ StaticR css_principal_css
+    
+    $(whamletFile "templates/nav.hamlet")
+    $(whamletFile "templates/footer.hamlet")
+        
